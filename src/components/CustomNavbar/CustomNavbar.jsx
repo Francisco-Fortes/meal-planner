b3fc21e10cf91a949797e0a694758afaa2f9efba
Francisco-Fortes <fortesfco@gmail.com>
2023-03-07 22:33:46 +0100

    Colors & Buttons testing

--- src/components/CustomNavbar/CustomNavbar.jsx
+++ src/components/CustomNavbar/CustomNavbar.jsx
@@ -3,37 +3,38 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import SignInModal from "../SignInModal/SignInModal";
+import SignInModal from "../SignInModal/SignInModal.jsx";
+import SignUpModal from "../SignUpModal/SignUpModal.jsx";
 
 function CustomNavbar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect className="main-color" expand="lg" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">APP-NAME</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            {/* This should be a Search Modal */}
+            <Nav.Item href="#SearchModal">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-search"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+              </svg>
+            </Nav.Item>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">
+            <Nav.Item>
               <SignInModal />
-            </Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <SignUpModal />
+            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
