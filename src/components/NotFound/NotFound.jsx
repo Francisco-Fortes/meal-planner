4c3ec32f83f28ad4ad3eb6ee32de8fe3e60aee49
Francisco-Fortes <fortesfco@gmail.com>
2023-03-17 23:55:14 +0100

    NotFound created

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+const NotFound = () => (
+  <Container>
+    <Row className="justify-content-center mt-5">
+      <Col>
+        <h2>PAGE NOT FOUND</h2>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default NotFound;
