163fd1dbffbdf9712c13a0eb2ccb4ae0846704b1
Francisco-Fortes <fortesfco@gmail.com>
2023-03-30 23:49:23 +0200

    Hero updated
    Logo added
    Test background image added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,7 @@
-import { Container, Row, Col } from "react-bootstrap";
 import "./Hero.css";
+import { Col } from "react-bootstrap";
 
 function Hero() {
-  return (
-    <Container fluid className="heroContainer">
-      <Row className="d-flex justify-content-center m-0 p-0">
-        <Col md="5">
-          <h2>ALL YOUR MEALS IN LESS THAN 3 MINUTES</h2>
-        </Col>
-      </Row>
-    </Container>
-  );
+  return <Col fluid className="hero-frame"></Col>;
 }
-
 export default Hero;
