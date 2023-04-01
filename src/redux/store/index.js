0672d8f38c3790dcddf8caf549e56af7a8c4460c
Francisco-Fortes <fortesfco@gmail.com>
2023-04-01 23:53:03 +0200

    Redux updated
    plannerReducer created and added to the store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,20 +1,24 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import allPlannerReducer from "../reducers/allPlannersReducer";
 import favouriteReducer from "../reducers/favouriteReducer";
+import plannerReducer from "../reducers/plannerReducer";
 import recipeReducer from "../reducers/recipeReducer";
 import userReducer from "../reducers/userReducer";
 
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["recipe", "favourite", "user"],
+  whitelist: ["recipe", "favourite", "user", "planner"],
 };
 
 const bigReducer = combineReducers({
   recipe: recipeReducer,
   favourite: favouriteReducer,
   user: userReducer,
+  planner: plannerReducer,
+  allPanners: allPlannerReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, bigReducer);
