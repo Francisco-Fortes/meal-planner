3070bd9dec353682cadb2487fbdaf74fffbca729
Francisco-Fortes <fortesfco@gmail.com>
2023-10-29 16:58:37 +0100

    Reducer updated
    delete, rename and duplicate added

--- src/redux/reducers/plannerReducer.js
+++ src/redux/reducers/plannerReducer.js
@@ -1,4 +1,10 @@
-import { GET_PLANNER, SAVE_PLANNER } from "../actions/index.js";
+import {
+  GET_PLANNER,
+  SAVE_PLANNER,
+  DELETE_PLANNER,
+  RENAME_PLANNER,
+  DUPLICATE_PLANNER,
+} from "../actions/index.js";
 import { GET_PLANNERS } from "../actions/index.js";
 const initialState = {
   planner: {},
@@ -22,6 +28,34 @@ const plannerReducer = (state = initialState, action) => {
         ...state,
         planners: action.payload,
       };
+    case DELETE_PLANNER:
+      return {
+        ...state,
+        planners: state.planners.filter(
+          (planner) => planner._id !== action.payload
+        ),
+      };
+    case RENAME_PLANNER:
+      const updatedPlanners = state.planners.map((planner) => {
+        if (planner._id === action.payload.plannerId) {
+          return {
+            ...planner,
+            title: action.payload.newTitle,
+          };
+        }
+        return planner;
+      });
+      return {
+        ...state,
+        planners: updatedPlanners,
+      };
+    case DUPLICATE_PLANNER:
+      return {
+        ...state,
+        // Add the duplicated planner to the list of planners
+        planners: [...state.planners, action.payload],
+      };
+
     default:
       return state;
   }
